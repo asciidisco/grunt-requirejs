31c86c4c67e9b5fd7e7aa11be165c44c0fc6d04c
asciidisco <public@asciidisco.com>
2012-05-07 22:28:23 +0200

    Version bump to 0.2.0 - read the changelog for more information

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Use [@cowboys](https://github.com/cowboy) wonderful js based optimizer [grunt](h
 together with [@jrburkes](https://github.com/jrburke) [r.js](https://github.com/jrburke/r.js) optimizer,
 to build your AMD based projects with grunt.
 
+Now with [almond](https://github.com/jrburke/almond) goodness.
+
+## Upgrade warning
+I removed the 'clearTarget' config option from the plugin, because i want you all
+to go to [grunt-contrib](https://github.com/gruntjs/grunt-contrib) page and use their
+'clean' task instead!
+
 ## Getting Started
 Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-requirejs`
 
@@ -13,11 +20,12 @@ Then add this line to your project's `grunt.js` gruntfile.
 task.loadNpmTasks('grunt-requirejs');
 ```
 
-
 ### Resources
 [grunt]: https://github.com/cowboy/grunt
 [getting_started]: https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
 [requirejs]: http://requirejs.org
+[almond]: https://github.com/jrburke/almond
+[grunt-contrib]: https://github.com/gruntjs/grunt-contrib
 
 ## Documentation
 Load the grunt-requirejs task as described in 'Getting started' and add your r.js optimizer
@@ -29,14 +37,13 @@ Example require js optimizer grunt file config entry:
 
 // ... grunt file contents
  requirejs: {
-      clearTarget: true,
       dir: 'build',
       appDir: 'src',
       baseUrl: 'js',
       paths: {
-          underscore          : '../vendor/underscore',
-          jquery              : '../vendor/jquery',
-          backbone            : '../vendor/backbone'
+          underscore: '../vendor/underscore',
+          jquery    : '../vendor/jquery',
+          backbone  : '../vendor/backbone'
       },
       pragmas: {
           doExclude: true
@@ -49,10 +56,58 @@ Example require js optimizer grunt file config entry:
 // ... even more grunt file contents
 ```
 
-The only difference to the std. r.js optimization config is, that you can add a 'clearTarget' flag
-to delete the contents of the target dir before building.
+You see, there is no difference in declaring your require config when your using your gruntfile
+instead of using a separate requirejs config file.
+
+## Almond
+If you like to replace require.js with almond.js during the build process, grunt-requirejs comes with an
+experimental [almond](https://github.com/jrburke/almond) injection mode. It even converts your require 
+<script> calls in your html files to call the "almondyfied" module, instead of calling require.js 
+that then calls (e.g.) loads the module.
+
+The only constraint for using the auto almond insertion is, that you at least define one module
+(mostly named as 'main').
+
+// ... grunt file contents
+ requirejs: {
+      // almond specific contents
+      // *insert almond in all your modules
+      almond: true,
+      // *replace require <script> calls, with the almond modules
+      // in the following files 
+      replaceRequireScript: [{
+        files: ['build/index.html'],
+        module: 'main'
+      }],
+      // "normal" require config
+      // *create at least a 'main' module
+      // thats necessary for using the almond auto insertion
+      modules: [{name: 'main'}],      
+      dir: 'build',
+      appDir: 'src',
+      baseUrl: 'js',
+      paths: {
+          underscore: '../vendor/underscore',
+          jquery    : '../vendor/jquery',
+          backbone  : '../vendor/backbone'
+      },
+      pragmas: {
+          doExclude: true
+      },
+      skipModuleInsertion: false,
+      optimizeAllPluginResources: true,
+      findNestedDependencies: true
+    }
+
+// ... even more grunt file contents
+```
 
 ## Release History
+### 0.2.0
++ Removed clearTarget (use grunt-contrib clean instead)
++ Added [almond](https://github.com/jrburke/almond) integration
++ Added automatic almond js module script tag replacement for html files
++ Imporoved documentation
 ### 0.1.0
 + Initial Release
 
--- tasks/require.js
+++ tasks/require.js
@@ -10,47 +10,67 @@ module.exports = function(grunt) {
   // Grunt utilities.
   var task = grunt.task;
   var file = grunt.file;
-  var utils = grunt.utils;
   var log = grunt.log;
   var verbose = grunt.verbose;
   var fail = grunt.fail;
-  var option = grunt.option;
   var config = grunt.config;
-  var template = grunt.template;
-  // requirejs optimizer
-  var rjs = require('requirejs');
-  var fs = require('fs');
-  var wrench = require('wrench');
+  var utils = typeof grunt.utils !== 'undefined' ? grunt.utils : grunt.util;
 
+  // shortcuts
+  var isArray = utils._.isArray;
+  var isFunction = utils._.isFunction;
+  var clone = utils._.clone;
+  
+  // lib dependencies
+  var rjs = require('requirejs'); 
+  var $ = require('jQuery');
+  var npm = require('npm');
+
+  // ==========================================================================
+  // PRIVATE HELPER FUNCTIONS
+  // ==========================================================================
+
+  // runs the require js optimizer
+  var optimize = function (config, done, cb) {
+    return function (chgConfig) {
+        grunt.helper('optimize', {
+            config: typeof chgConfig === 'undefined' ? config : chgConfig,
+            done: done,
+            cb: cb
+        });      
+    }
+  };
+
+  // runs the almond js html file replacement
+  var replaceAlmond = function (config, done, cb) {
+    return function (chgConfig) {
+        grunt.helper('replaceRequireWithAlmond', {
+            config: typeof chgConfig === 'undefined' ? config : chgConfig,
+            done: done,
+            cb: cb
+        });
+    }
+  };
 
   // ==========================================================================
   // TASKS
   // ==========================================================================
 
-  grunt.registerTask('requirejs', 'Runs requirejs optimizer', function() {
+  grunt.registerMultiTask('requirejs', 'Runs requirejs optimizer', function() {
     var done = this.async(),
-        optimize = function () {
-          // run the optimizer
-          grunt.helper('optimize', {
-              config: config.get('requirejs'),
-              done: function(err) {
-                  done();
-              }
-          });
-        }
+        rqConfig = this.data;
 
     // log process start
-    log.writeln('>> RequireJS optimizer startet');
+    log.ok('RequireJS optimizer started');
 
-    // check if we should clear the build directory
-    if (config.get('requirejs').clearTarget === true) {
-      grunt.helper('clearTarget', {
-        config: config.get('requirejs'),
-        cb: optimize
-      })
-    } else {
-      optimize();
-    }
+    // execute clear target helper
+    grunt.helper('almond', {
+      config: rqConfig,
+      done: done,
+      cb: optimize(rqConfig, done, replaceAlmond(rqConfig, done))
+    });
+    
+    return done;
   });
 
   // ==========================================================================
@@ -58,28 +78,130 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   // helper to execute requirejs optimizer function
-  grunt.registerHelper('optimize', function(options) {
+  grunt.registerHelper('optimize', function (options) {
+    // call rjs optimizer 
     rjs.optimize(options.config, function (result) {
       // check if verbose flag is set, then log result
-      if (verbose === true) {
-        log.writeln(result);
-      }
+      grunt.verbose.ok(result);
 
       // log process end
-      log.writeln('>> RequireJS optimizer finished');
+      log.ok('RequireJS optimizer finished');
 
-      options.done();
+      // check for callback, else mark as done
+      if (isFunction(options.cb)) {
+        options.cb();
+      } else {
+        options.done();
+      }
+
     });
   });
 
-  // helper to clean the build target directory
-  grunt.registerHelper('clearTarget', function (options) {
-    // log clear target function
-    log.writeln('>> Clear target directory');
-    wrench.rmdirSyncRecursive(options.config.dir, true);
-    fs.mkdirSync(options.config.dir);
-    options.cb();
-    return true;
+  // helper to execute requirejs optimizer function
+  grunt.registerHelper('replaceRequireWithAlmond', function (options) {
+    // check if we should replace require with almond in html files
+    if (options.config.almond === true && isArray(options.config.replaceRequireScript)) {
+      var $ = require('jQuery').create();
+
+      // iterate over all modules that are configured for replacement
+      options.config.replaceRequireScript.forEach(function (entry, idx) {
+        var files = grunt.file.expand(entry.files);
+        // log almond including
+        log.ok('Replacing require script calls, with almond module files');
+
+        // iterate over found html files
+        files.forEach(function (file, index) {
+          // load file contents
+          var contents = String(grunt.file.read(file, 'utf-8'));
+          // iterate over content nodes to find the correct script tags
+          $(contents).each(function (idx, elm) {
+
+            // check for require js like script tags
+            if (String(elm.tagName).toLowerCase() === 'script' && $(elm).attr('data-main') !== '') {
+              // replace the attributes of requires script tag
+              // with the 'almonded' version of the module
+              var $newElm = $(elm).clone(), newElm = $newElm[0].outerHTML;
+              contents = contents.replace($.trim(newElm.replace('<script', '').replace('></script>', '')), 'src="' + $newElm.attr('data-main') + '.js"');
+            }
+
+          });
+
+          // write out newly created file contents
+          grunt.file.write(file, contents, 'utf-8');
+        });
+      });
+
+      // check for callback, else mark as done
+      if (isFunction(options.cb)) {
+        options.cb();
+      } else {
+        options.done();
+      }
+
+    } else {
+
+      // check for callback, else mark as done      
+      if (isFunction(options.cb)) {
+        options.cb();
+      } else {
+        options.done();
+      }
+
+    }
+  });
+
+  // helper to execute requirejs optimizer function
+  grunt.registerHelper('almond', function (options) {
+    // check if we should inline almond
+    if (options.config.almond === true) {
+      var npmDir = npm.dir,
+          almondPath = npmDir + '/' + 'grunt-requirejs/node_modules/almond/',
+          configClone = clone(options.config);
+
+      // fetch the almond main file
+      npm.load(JSON.parse(grunt.file.read(almondPath + 'package.json')), function (err, npmLocal) {
+        
+        // log almond including
+        log.ok('Including almond.js');
+
+        // set almond path
+        configClone.paths.almond = String(almondPath + npmLocal.config.get('main')).replace('.js', '');
+
+        // modify modules data
+        configClone.modules.forEach(function (module, idx) {
+          // log adding of almond
+          grunt.verbose.ok('Adding almond to module: ' + module.name);
+
+          // check if the module has its own includes
+          // then append almond to them
+          // else generate a new includes property
+          if (isArray(module.include) === true) {
+            configClone.modules[idx].include.push('almond');
+          } else {
+            configClone.modules[idx].include = ['almond'];
+          }
+
+          // check for callback, else mark as done           
+          if (isFunction(options.cb)) {
+            options.cb(configClone);
+          } else {
+            options.done();
+          }
+
+        });
+
+      });
+
+    } else {
+
+      // check for callback, else mark as done
+      if (isFunction(options.cb)) {
+        options.cb();
+      } else {
+        options.done();
+      }
+
+    }
   });
 
 };
