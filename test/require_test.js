9f0fbab7e482615a7d0a5edbaa2cc18c6238b148
asciidisco <public@asciidisco.com>
2012-09-13 15:43:35 +0200

    Changed test cases

--- test/require_test.js
+++ test/require_test.js
@@ -12,18 +12,18 @@ exports['require'] = {
     var helperValuesSeriesA = grunt.helper('require_size_info', 'a', true, 'foo'),
         helperValuesSeriesB = grunt.helper('require_size_info', 'b', false, 'foobar');
     test.expect(8);
-    
+
     // tests here
-    test.equal(helperValuesSeriesA.module, 'a', 'Should determine the module');    
+    test.equal(helperValuesSeriesA.module, 'a', 'Should determine the module');
     test.equal(helperValuesSeriesA.gzipSize, 23, 'Should determine the correct gzip size');
     test.equal(helperValuesSeriesA.fileSize, 3, 'Should determine the correct "file" size');
     test.equal(helperValuesSeriesA.message, 'Compressed size for module "a": \u001b[32m23\u001b[39m bytes gzipped (\u001b[32m3\u001b[39m bytes minified).', 'Should output the correct message');
 
-    test.equal(helperValuesSeriesB.module, 'b', 'Should determine the module');    
+    test.equal(helperValuesSeriesB.module, 'b', 'Should determine the module');
     test.equal(helperValuesSeriesB.gzipSize, 26, 'Should determine the correct gzip size');
     test.equal(helperValuesSeriesB.fileSize, 6, 'Should determine the correct "file" size');
     test.equal(helperValuesSeriesB.message, 'Compressed size for module "b": \u001b[32m26\u001b[39m bytes gzipped (\u001b[32m6\u001b[39m bytes uncompressed).');
-    
+
     test.done();
   }
 };
