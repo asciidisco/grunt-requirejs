d4465ea5df3091caf2beac9f1254a04cdf3f9ee5
asciidisco <public@asciidisco.com>
2012-12-14 16:31:45 +0100

    Removed node_modules from examples

--- examples/almond-text-plugin-project/node_modules/grunt/tmp/Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt) {
-
-  // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
-
-};
