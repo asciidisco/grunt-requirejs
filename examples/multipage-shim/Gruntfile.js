9acc8538d872dd5a1595ceca90541206a9bdc8d3
Blaine Bublitz <blaine@iceddev.com>
2013-09-03 15:12:10 -0700

    remove www-build because it is named www-built

--- examples/multipage-shim/Gruntfile.js
+++ examples/multipage-shim/Gruntfile.js
@@ -85,6 +85,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
-  grunt.registerTask('default', ['jshint', 'qunit']);
+  grunt.registerTask('default', ['jshint', 'build']);
   grunt.registerTask('build', 'requirejs');
 };
